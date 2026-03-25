2d25be0d57988ba313c9deff31c52a1010ca89af
edoardocanzi-ops <edoardo.canzi@gmail.com>
2026-03-25 21:46:22 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'foundation-archive-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/1000013824.png',
+  '/1000013825.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
